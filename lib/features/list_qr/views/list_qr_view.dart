b8ec99a80998a489aec49aac2effa458cc634a5c
Aryosetowmn <aryosetowmnwork@gmail.com>
2025-11-18 12:08:07 +0700

    add list_qr page

---
+++ lib/features/list_qr/views/list_qr_view.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class MenuQrPage extends StatelessWidget {
+  const MenuQrPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        centerTitle: true,
+        title: const Text("Menu QR"),
+        elevation: 0,
+      ),
+
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Container(
+  margin: const EdgeInsets.only(bottom: 16),
+  padding: const EdgeInsets.all(12),
+  decoration: BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(16),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black12,
+        blurRadius: 4,
+        offset: const Offset(0, 2),
+      ),
+    ],
+  ),
+  child: Row(
+    children: [
+      // ICON QR
+      Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          color: const Color(0xFFEFEFFE),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Icon(
+          Icons.qr_code_2,
+          size: 40,
+          color: Color(0xFF6366F1),
+        ),
+      ),
+
+      const SizedBox(width: 12),
+
+      // Text Description
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Laqisya Gym",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: const [
+                Icon(Icons.location_on_outlined,
+                    size: 16, color: Colors.grey),
+                SizedBox(width: 4),
+                Expanded(
+                  child: Text(
+                    "Jl. Sudirman No. 123, Jakarta",
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
+
+        },
+      ),
+    );
+  }
+}
